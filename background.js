b85acf1938486da7dd6e5da62f3c61b3dc7f9d5f
Rajnish <b421037@iiit-bh.ac.in>
2023-07-10 22:56:23 +0530

    background.js file updated

--- background.js
+++ background.js
@@ -1,27 +1,15 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.executeScript(tab.id, {
-    code: `
-            var overlay = document.createElement('div');
-            overlay.style.position = 'fixed';
-            overlay.style.top = '0';
-            overlay.style.left = '0';
-            overlay.style.width = '100%';
-            overlay.style.height = '100%';
-            overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-            overlay.style.zIndex = '9999';
-            overlay.style.display = 'flex';
-            overlay.style.justifyContent = 'center';
-            overlay.style.alignItems = 'center';
-      
-            var message = document.createElement('h1');
-            message.style.color = '#fff';
-            message.style.fontFamily = 'Arial, sans-serif';
-            message.style.fontSize = '36px';
-            message.style.textAlign = 'center';
-            message.textContent = 'Browser Locked';
-      
-            overlay.appendChild(message);
-            document.body.appendChild(overlay);
-          `,
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ time: 0 });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  chrome.storage.sync.get(["time", "password"], function (result) {
+    const time = result.time;
+    const password = result.password;
+    if (time > 0 && changeInfo.status === "complete") {
+      setTimeout(function () {
+        chrome.tabs.update(tabId, { url: "password_authentication.html" });
+      }, time * 60000);
+    }
   });
 });
