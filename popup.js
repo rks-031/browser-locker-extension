bae694fe55e6f6f3ced498d65b37ca5e5085add2
Rajnish <b421037@iiit-bh.ac.in>
2023-07-10 21:15:19 +0530

    boiler-plate code written

---
+++ popup.js
@@ -0,0 +1,29 @@
+document.getElementById("lockButton").addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: `
+              var overlay = document.createElement('div');
+              overlay.style.position = 'fixed';
+              overlay.style.top = '0';
+              overlay.style.left = '0';
+              overlay.style.width = '100%';
+              overlay.style.height = '100%';
+              overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+              overlay.style.zIndex = '9999';
+              overlay.style.display = 'flex';
+              overlay.style.justifyContent = 'center';
+              overlay.style.alignItems = 'center';
+      
+              var message = document.createElement('h1');
+              message.style.color = '#fff';
+              message.style.fontFamily = 'Arial, sans-serif';
+              message.style.fontSize = '36px';
+              message.style.textAlign = 'center';
+              message.textContent = 'Browser Locked';
+      
+              overlay.appendChild(message);
+              document.body.appendChild(overlay);
+            `,
+    });
+  });
+});
