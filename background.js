250bb625f7abdce4fdef90d9f1afa962ccc8344c
Rajnish <b421037@iiit-bh.ac.in>
2023-07-15 23:37:10 +0530

    Update background.js

--- background.js
+++ background.js
@@ -2,14 +2,12 @@ const second = 1000;
 
 console.log("LockBrowser activated!");
 
-// Querying for the active tab
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   if (tabs[0]) {
     const activeTab = tabs[0];
     if (activeTab.id) {
       chrome.tabs.get(activeTab.id, function (tab) {
         if (tab) {
-          // Retrieving the stored password and time from storage
           chrome.storage.sync.get(["password", "time"], (res) => {
             const password = res.password;
             const time = res.time;
@@ -20,25 +18,30 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             if (time === undefined) {
               console.log("Time is undefined. Please set a valid time.");
             } else {
-              // Setting a timeout to lock the browser after the specified time
               setTimeout(() => {
-                // Creating a new window for password authentication
                 chrome.windows.create(
                   { url: "password_authentication.html" },
                   function (window) {
                     const windowId = window.id;
 
-                    // Blocking web navigation and new tab creation
+                    // Blocking web navigation to existing tabs
+                    //Not working
                     chrome.webNavigation.onBeforeNavigate.addListener(
                       (details) => {
-                        if (details.tabId === activeTab.id) {
-                          chrome.tabs.update(activeTab.id, {
+                        if (details.tabId !== activeTab.id) {
+                          chrome.tabs.update(details.tabId, {
                             url: "password_authentication.html",
                           });
+                          chrome.tabs.remove(details.tabId);
                         }
-                        chrome.tabs.remove(details.tabId);
-                      }
+                      },
+                      { url: [{ urlMatches: "<all_urls>" }] }
                     );
+
+                    // Blocking creation of new tabs
+                    chrome.tabs.onCreated.addListener((newTab) => {
+                      chrome.tabs.remove(newTab.id);
+                    });
 
                     // Unlocking the browser when the correct password is entered
                     chrome.runtime.onMessage.addListener(
